cd5aba4ce94f22e677a94da1c8d3c300912702a7
Frederico Drumond <fredrumond@gmail.com>
2016-11-22 22:39:26 -0200

    [UP] Classe Produto

--- _app/Classes/Produto.class.php
+++ _app/Classes/Produto.class.php
@@ -2,78 +2,80 @@
 
 class Produto {
 
-    private $Codigo;
-    private $Nome;
-    private $Descricao;
-    private $TipoUnitario;
-    private $Fornecedor;
-    private $EstoqueMinimo;
-    private $EstoqueInicial;
-    private $EstoqueAtual;
+    private $Cod_pro;
+    private $Nom_pro;
+    private $Cat_pro;
+    private $For_pro;
+    private $Est_min_pro;
+    private $Est_ini_pro;
+    private $Est_atu_pro;
 
-    function __construct($Nome, $EstoqueMinimo, $EstoqueInicial) {
-        $this->Nome = $Nome;
-        $this->EstoqueMinimo = $EstoqueMinimo;
-        $this->EstoqueInicial = $EstoqueInicial;
-        $this->EstoqueAtual = $EstoqueInicial;
+    function __construct($Nom_pro, $Cat_pro, $For_pro, $Est_min_pro, $Est_ini_pro) {
+        $this->Nom_pro = $Nom_pro;
+        $this->Cat_pro = $Cat_pro;
+        $this->For_pro = $For_pro;
+        $this->Est_min_pro = $Est_min_pro;
+        $this->Est_ini_pro = $Est_ini_pro;
+        $this->Est_atu_pro = $Est_ini_pro;
     }
 
     public function cadastraProduto(Produto $p) {
         $create = new Create();
         $ArrProduto = [
-            'nome' => $this->Nome,
-            'estoque_minimo' => $this->EstoqueMinimo,
-            'estoque_inicial' => $this->EstoqueInicial,
-            'estoque_atual' => $this->EstoqueAtual
+            'nom_pro' => $this->Nom_pro,
+            'cat_pro' => $this->Cat_pro,
+            'for_pro' => $this->For_pro,
+            'est_min_pro' => $this->Est_min_pro,
+            'est_ini_pro' => $this->Est_ini_pro,
+            'est_atu_pro' => $this->Est_atu_pro
         ];
         $create->ExeCreate('estoque', $ArrProduto);
     }
 
-    public function retirarProduto($Codigo, $Qtd) {
-        $read = new Read();
+    public function movimentacaoProduto($cod_pro, $descrição, $qtd, $valor, $tipo_mov) {
         $create = new Create();
-        $update = new Update();
-        $read->ExeRead('estoque');
+        $ArrProduto = [
+            'cod_pro' => $cod_pro,
+            'des_mov' => $descrição,
+            'qtd_mov' => $qtd,
+            'vlr_mov' => $valor,
+            'tipo_mov' => $tipo_mov
+        ];
+        $create->ExeCreate('movimentacao', $ArrProduto);
 
-        if ($read->getRowCount() != 0):
-            foreach ($read->getResult() as $p):
-                if ($Codigo == $p['codigo']):
-                    $ArrProduto = [
-                        'cod_produto' => $Codigo,
-                        'quantidade' => $Qtd,
-                    ];
-                    $create->ExeCreate('saida', $ArrProduto);
-                    $atual = $p['estoque_atual'];
-                    $ArrAtualiza = ['estoque_atual' => $atual - $Qtd];
-                    $update->ExeUpdate('estoque', $ArrAtualiza, "WHERE codigo = :cod", 'cod=' . $Codigo);
-                endif;
-            endforeach;
+        if ($tipo_mov == 'entrada'):
+            $this->adicionarProduto($cod_pro, $qtd);
+        elseif ($tipo_mov == 'saida'):
+            $this->retirarProduto($cod_pro, $qtd);
         else:
-            echo "Não tem produtos no estoque!";
+            echo "Nao encontrado";
         endif;
     }
 
     public function adicionarProduto($Codigo, $Qtd) {
         $read = new Read();
-        $create = new Create();
-        $update = new Update();
-
         $read->ExeRead('estoque');
 
-        $ArrProduto = ['cod_produto' => $Codigo, 'quantidade' => $Qtd];
+        foreach ($read->getResult() as $produto):
+            $atual = $produto['est_atu_pro'];
+        endforeach;
 
-        if ($read->getRowCount() != 0):
-            foreach ($read->getResult() as $p):
-                if ($Codigo == $p['codigo']):
-                    $create->ExeCreate('entrada', $ArrProduto);
-                    $atual = $p['estoque_atual'];
-                    $ArrAtualiza = ['estoque_atual' => $atual + $Qtd];
-                    $update->ExeUpdate('estoque', $ArrAtualiza, "WHERE codigo = :cod", 'cod=' . $Codigo);
-                endif;
-            endforeach;
-        else:
-            echo "Não tem produto cadastrado no estoque pelo codigo informado!";
-        endif;
+        $update = new Update();
+        $ArrUpdate = ['est_atu_pro' => $atual + $Qtd];
+        $update->ExeUpdate('estoque', $ArrUpdate, "WHERE cod_pro = :cod", 'cod=' . $Codigo);
+    }
+
+    public function retirarProduto($Codigo, $Qtd) {
+        $read = new Read();
+        $read->ExeRead('estoque');
+
+        foreach ($read->getResult() as $produto):
+            $atual = $produto['est_atu_pro'];
+        endforeach;
+
+        $update = new Update();
+        $ArrUpdate = ['est_atu_pro' => $atual - $Qtd];
+        $update->ExeUpdate('estoque', $ArrUpdate, "WHERE cod_pro = :cod", 'cod=' . $Codigo);
     }
 
     public function Real($Valor) {
--- index.php
+++ index.php
@@ -11,25 +11,24 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        require('./_app/Config.inc.php');  
+        require('./_app/Config.inc.php');
         
-        $produto = new Produto('Chinelo', 5, 20);
-        //$produto->cadastraProduto($produto);        
+        
+        $produto = new Produto('Chinelo', 'Calçado', 'Nike', 5, 20);
+        //$produto->cadastraProduto($produto); 
            
         $leProdutos = new Read();
         $leProdutos->ExeRead('estoque');
         
         if($leProdutos->getRowCount() >= 1):            
             foreach ($leProdutos->getResult() as $p):
-                echo $p['codigo'] . " | " . $p['nome'] . " | " . $p['estoque_minimo'] . " | " . $p['estoque_inicial'] . " | " . $p['estoque_atual'] . "<br/>";
+                echo $p['cod_pro'] . " | " . $p['nom_pro'] . " | " . $p['cat_pro'] . " | " . $p['for_pro'] . " | " . $p['est_min_pro'] . " | " . $p['est_ini_pro'] . " | " . $p['est_atu_pro'] . "<br/>";
             endforeach;
         else:
             echo "Não tem produtos inseridos no estoque";
         endif;
-        //var_dump($leProdutos->getResult());
         
-        //$produto->adicionarProduto(5,100);
-        //$produto->retirarProduto(3, 100);
+        //$produto->movimentacaoProduto(9, 'bla bla bla', 2, 39.90, 'saida');
         echo "<hr>";
         
         $preco = 5.80;
