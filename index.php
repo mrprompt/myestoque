2aadb429f31326ab8ba278eed831cb338dae30ba
Frederico Drumond <fredrumond@gmail.com>
2016-12-01 16:17:18 -0200

    [UP] Index explorando o Silex

--- index.php
+++ index.php
@@ -4,9 +4,25 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->get('/', function(){
-    return 'Olá mundo!';
-});
+/* CONEXAO COM O BANCO */
+function getConn() {
+    $dsn = 'mysql:host=localhost;dbname=myestoque';
+    try {
+        return $dbh = new PDO($dsn, 'root', '');
+    } catch (PDOException $e) {
+        echo 'Connection failed: ' . $e->getMessage();
+    }
+}
+
+//PEGA PRODUTOS NO ESTOQUE
+function getEstoque() {
+    $stmt = getConn()->query("SELECT * FROM estoque");
+    $estoque = $stmt->fetchAll(PDO::FETCH_OBJ);
+    echo json_encode(array('estoque' => $estoque));
+}
+
+#ROTA PARA ESTOQUE
+$app->get('/estoque','getEstoque');
 
 $app->run();
 
